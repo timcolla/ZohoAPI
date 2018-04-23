1bd9ccb9c2de8518df5a0154c0d23b7c623c882a
Tim Colla <tim.colla@marinosoftware.com>
2018-04-23 11:39:50 +0100

    Added encodeURIComponent to all URI components. Updated error messages and variable names to be more consistent.

--- server.js
+++ server.js
@@ -29,15 +29,15 @@ app.get('/about', function(req, res) {
 
 // setup page 
 app.get('/setup', function(req, res) {
-    res.render('pages/setup', { email: email, authtoken: authToken});
+    res.render('pages/setup', { email: email, authToken: authToken});
 });
 
 app.post('/setup', urlencodedParser, function(req, res) {
-	if (!req.body.email || !req.body.authtoken) return res.sendStatus(400)
+	if (!req.body.user || !req.body.authToken) return res.sendStatus(400)
 
-	authToken = req.body.authtoken;
-	email = req.body.email;
-    res.render('pages/setup', { email: email, authtoken: authToken});
+	authToken = encodeURIComponent(req.body.authToken);
+	email = encodeURIComponent(req.body.user);
+    res.render('pages/setup', { email: email, authToken: authToken});
 });
 
 // Projects page
@@ -69,10 +69,9 @@ app.get('/projects', function(req, res) {
 });
 
 app.post('/projects', function(req, res) {
-	if (req.query.authToken) {
-		authToken = req.query.authToken;
-	} 
-	if (authToken == "") return res.send({ error: "Auth token isn't set."});
+	authToken = encodeURIComponent(req.query.authToken) || "";
+
+	if (authToken == "") return res.send({ error: "authToken isn't set."});
 
 	getProjects(authToken, function(result) {
 		res.send(result);
@@ -105,7 +104,7 @@ app.get(['/project', '/project/:project'], function(req, res) {
 	if (authToken == "") return res.render(page, { error: "authToken not set, go to /setup first." });
 	if (!req.params.project) return res.render(page, { error: "Project id missing. Go to /project/<ProjectId>." });
 
-	var projectID = req.params.project;
+	var projectID = encodeURIComponent(req.params.project);
 
 	getJobs(authToken, email, projectID, function(result) {
 		res.render(page, result);
@@ -113,19 +112,13 @@ app.get(['/project', '/project/:project'], function(req, res) {
 });
 
 app.post('/project', function(req, res) {
-	var projectID = "";
-	if (req.query.authToken) {
-		authToken = req.query.authToken;
-	} 
-	if (req.query.email) {
-		email = req.query.email;
-	}
-	if (req.query.projectID) {
-		projectID = req.query.projectID;
-	}
-	if (authToken == "") return res.send({ error: "Auth token isn't set."});
-	if (email == "") return res.send({ error: "Email isn't set."});
-	if (projectID == "") return res.send({ error: "Project isn't set."});
+	authToken = encodeURIComponent(req.query.authToken) || "";
+	email = encodeURIComponent(req.query.user) || "";
+	var projectID = encodeURIComponent(req.query.projectID) || "";
+
+	if (authToken == "") return res.send({ error: "authToken isn't set."});
+	if (email == "") return res.send({ error: "user isn't set."});
+	if (projectID == "") return res.send({ error: "projetID isn't set."});
 
 	getJobs(authToken, email, projectID, function(result) {
 		res.send(result);
@@ -143,14 +136,14 @@ app.post('/addtimelog', function(req, res) {
 	var billingStatus = encodeURIComponent(req.query.billingStatus) || "";
 	var description = encodeURIComponent(req.query.description) || "";
 
-	if (authToken == "") return res.send({ error: "Auth token isn't set."});
-	if (email == "") return res.send({ error: "Email isn't set."});
+	if (authToken == "") return res.send({ error: "authToken isn't set."});
+	if (email == "") return res.send({ error: "user isn't set."});
 	if (projectID == "") return res.send({ error: "projetID isn't set."});
 	if (jobID == "") return res.send({ error: "jobID isn't set."});
 	if (workDate == "") return res.send({ error: "workDate isn't set."});
 	if (hours == "") return res.send({ error: "hours isn't set."});
 	if (billingStatus == "") return res.send({ error: "billable isn't set."});
-console.log(description);
+
 	request('https://people.zoho.eu/people/api/timetracker/addtimelog?authtoken='+authToken+'&user='+email+'&projectId='+projectID+'&jobId='+jobID+'&workDate='+workDate+'&hours='+hours+'&billingStatus='+billingStatus+'&description='+description, function (error, response, body) {
 
 		if (!error) {
@@ -169,22 +162,6 @@ console.log(description);
 		}
 	});
 });
-
-// POST /api/users gets JSON bodies
-app.post('/api/users', jsonParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400)
-  // create user in req.body
-})
-
-// var request = require('request');
-// request('https://people.zoho.eu/people/api/timetracker/getprojects?authtoken=2e40ac7bbae44fce1a9a94978c143379', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-
-//   var json = JSON.parse(body);
-//   console.log(json);
-// });
 
 app.listen(8080);
 console.log('8080 is the magic port');
